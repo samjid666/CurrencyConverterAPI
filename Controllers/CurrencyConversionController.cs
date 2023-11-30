9a70d324c82aa0a20584c44ca48276de90b28273
samjid666 <samjidali@gmail.com>
2023-11-30 17:23:56 +0530

    New changes

---
+++ Controllers/CurrencyConversionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CurrencyConversionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult ConvertCurrency(decimal amount, string fromCurrency, string toCurrency)
+    {
+        // Your currency conversion logic here
+        // Example: Convert from USD to EUR using a fixed rate
+        decimal conversionRate = 0.85M;
+        decimal convertedAmount = amount * conversionRate;
+
+        return Ok(new { Amount = convertedAmount, FromCurrency = fromCurrency, ToCurrency = toCurrency });
+    }
+}
